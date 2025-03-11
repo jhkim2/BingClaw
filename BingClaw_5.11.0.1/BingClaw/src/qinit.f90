fd43131ecacc3e76ba1b9d93f5f72f55f931c7df
jihwan <jhkim2@uw.edu>
2025-03-11 17:54:48 +0000

    updated qinit.f90

--- BingClaw_5.11.0.1/BingClaw/src/qinit.f90
+++ BingClaw_5.11.0.1/BingClaw/src/qinit.f90
@@ -64,7 +64,11 @@ subroutine qinit(meqn,mbc,mx,my,xlow_patch,ylow_patch,dx,dy,q,maux,aux)
                         dq = dq / ((xipc-ximc)*(yjpc-yjmc))
                     endif
 
-                    q(6,i,j) = q(6,i,j) + dq
+                    if q(1,i,j)>0. then
+                        q(6,i,j) = q(6,i,j) + dq
+                    else
+                        q(6,i,j) = 0.d0
+                    endif
 
                 endif
             enddo
